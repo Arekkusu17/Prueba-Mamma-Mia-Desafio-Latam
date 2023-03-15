26f47ff717916f6e1d6c42b025a7e3de4ca54099
Arekkusu17 <fernn.alex@gmail.com>
2023-03-15 00:49:15 -0300

    change to useContext and not props

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -1,18 +1,23 @@
-import React, { useState } from "react";
+import React from "react";
 import { useNavigate } from "react-router-dom";
 import "../assets/css/gallery.css";
+import { CartContext } from "../context/CartProvider";
+import { MenuContext } from "../context/MenuProvider";
 
-export default function Gallery({ menuList }) {
+export default function Gallery() {
+
+  // const { }
+
+  const { menuList } = React.useContext(MenuContext)
+  const { addItemToCart, formatChileanCurrency } = React.useContext(CartContext)
 
   const navigate = useNavigate()
 
-  function formatChileanCurrency(number) {
-    const formatter = new Intl.NumberFormat('es-CL', { style: 'currency', currency: 'CLP' });
-    return formatter.format(number);
+  const handleClickAddItem = (id) => {
+    addItemToCart(id)
   }
-
   const handleClickViewMore = (id) => {
-    navigate(`/pizza/:${id}`)
+    navigate(`/pizza/${id}`)
   }
 
   return (
@@ -23,6 +28,7 @@ export default function Gallery({ menuList }) {
           <img src={pizza.img} className="card-img-top" alt="..." />
           <div className="card-body">
             <h3 className="card-title">{pizza.name}</h3>
+
             <p className="card-text">Ingredientes:</p>
             <ul>
               {pizza.ingredients.map((ingrediente, idx) => (
@@ -33,7 +39,7 @@ export default function Gallery({ menuList }) {
           </div>
           <div className="buttons">
             <button className="btn btn-primary" onClick={() => { handleClickViewMore(pizza.id) }}>Ver Más 👀</button>
-            <button className="btn btn-danger"> Añadir 🛒</button>
+            <button className="btn btn-danger" onClick={() => { handleClickAddItem(pizza.id) }}> Añadir 🛒</button>
           </div>
         </div>
       ))
