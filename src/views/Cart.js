a550161094685a0d187259cb535b6d1626f1ff13
Arekkusu17 <fernn.alex@gmail.com>
2023-03-16 02:36:13 -0300

    add an alert and a cart empty render

--- src/views/Cart.js
+++ src/views/Cart.js
@@ -1,4 +1,6 @@
 import React from 'react'
+import { Link, useNavigate } from 'react-router-dom'
+import Swal from 'sweetalert2'
 import '../assets/css/cart.css'
 import { CartContext } from '../context/CartProvider'
 import { MenuContext } from '../context/MenuProvider'
@@ -26,27 +28,49 @@ export default function Cart() {
   const handleClickRemoveOneItem = (id) => {
     decreaseItemFromCart(id)
   }
+
+  const navigate = useNavigate()
   return (
-    <section className='cart-section'>
-      <h2>Detalles del pedido:</h2>
-      <div className='order-resume w-100 p-1'>
-        {cartItems.map((item) => (
-          <div className='order-item ' key={item.id}>
-            <div className='d-flex flex-row align-items-center'>
-              <img src={getItemFromMenu(item.id, menuList).img} alt="" className='img-fluid' />
-              <p className='fs-5 my-1'>{getItemFromMenu(item.id, menuList).name}</p>
-            </div>
-            <div className='d-flex flex-row align-items-center'>
-              <p className='fs-5 m-2 text-success price-per-item'>{formatChileanCurrency(totalPricePerItem(item.id, cartItems, menuList))}</p>
-              <button className='btn btn-danger' onClick={() => { handleClickRemoveOneItem(item.id) }}>-</button>
-              <p className='fs-4 m-2'>{item.quantity}</p>
-              <button className='btn btn-primary' onClick={() => { handleClickAddItem(item.id) }}>+</button>
-            </div>
-          </div>
-        ))}
-        <h3 className='fs-1'>Total: {formatChileanCurrency(totalPrice)}</h3>
-        <button className='btn btn-success'>Ir a Pagar</button>
-      </div>
-    </section>
+    <>
+      <section className='cart-section'>
+        <h2>Detalles del pedido:</h2>
+        <div className='order-resume w-100 p-1'>
+          {cartItems.length <= 0 ? (
+            <>
+              <p className='fs-1'>Tu carrito está vacío😢</p>
+              <button className='btn btn-success' onClick={() => { navigate('/home') }}>Volver a Inicio</button>
+            </>
+          ) : (
+            <>{
+              cartItems.map((item) => (
+                <div className='order-item ' key={item.id}>
+                  <div className='d-flex flex-row align-items-center'>
+                    <img src={getItemFromMenu(item.id, menuList).img} alt="" className='img-fluid' />
+                    <p className='item-name fs-5 my-1'>{getItemFromMenu(item.id, menuList).name}</p>
+                  </div>
+                  <div className='d-flex flex-row align-items-center'>
+                    <p className='fs-5 m-2 text-success price-per-item'>{formatChileanCurrency(totalPricePerItem(item.id, cartItems, menuList))}</p>
+                    <button className='btn btn-danger' onClick={() => { handleClickRemoveOneItem(item.id) }}>-</button>
+                    <p className='fs-4 m-2'>{item.quantity}</p>
+                    <button className='btn btn-primary' onClick={() => { handleClickAddItem(item.id) }}>+</button>
+                  </div>
+                </div>
+              ))}
+              <h3 className='fs-1'>Total: {formatChileanCurrency(totalPrice)}</h3>
+              <Link className='btn btn-danger' to='/home'>Volver a Inicio</Link>
+              <button className='btn btn-success ms-4' onClick={() => {
+                Swal.fire({
+                  icon: 'success',
+                  title: 'Se ha realizado el pago!',
+                  text: 'Muchas gracias por confiar en nuestros productos.'
+                })
+              }}>Ir a Pagar</button>
+            </>
+          )}
+
+        </div>
+
+      </section>
+    </>
   )
 }
